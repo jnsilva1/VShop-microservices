d8c1eb4c752066c45d7822f27e81357a39b4a024
José Silva <josenilo.cp@gmail.com>
2023-09-10 18:28:19 -0300

    Resolving navigation property for json serialization

--- VShop.ProductApi/DTOs/ProductDTO.cs
+++ VShop.ProductApi/DTOs/ProductDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace VShop.ProductApi;
 
@@ -24,6 +25,8 @@ public class ProductDTO
     [Range(1, 9999)]
     public long Stock { get; set; }
     public string? ImageURL { get; set; }
+
+    [JsonIgnore]
     public Category? Category { get; set; }
     public int CategoryId { get; set; }
 }
