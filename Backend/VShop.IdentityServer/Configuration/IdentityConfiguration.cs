43588f138361371100c3f369be1ff4246d2caf29
José Silva <josenilo.cp@gmail.com>
2023-09-20 22:32:02 -0300

    settingup identity server

---
+++ Backend/VShop.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace VShop.IdentityServer.Configuration;
+
+public class IdentityConfiguration
+{
+    public const string Admin = "Admin";
+    public const string Client = "Client";
+
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource>{
+            new IdentityResources.OpenId(),
+            new IdentityResources.Email(),
+            new IdentityResources.Profile()
+        };
+
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>{
+            new(name: "vshop", displayName: "VShop Server"),
+            new(name: "read", displayName: "Read Data"),
+            new(name: "write", displayName: "Write Data"),
+            new(name: "delete", displayName: "Delete Data")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>{
+            new(){
+                ClientId = "client",
+                ClientSecrets = { new Secret("abracadabra#simsalabim".Sha256())},
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = {"read", "write", "profile"}
+            },
+            new(){
+                ClientId =  "vshop",
+                ClientSecrets = { new Secret("abracadabra#simsalabim".Sha256())},
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = {"https://localhost:7103/signin-oidc"},
+                PostLogoutRedirectUris = {"https://localhost:7103/signout-callback-oidc"},
+                AllowedScopes = {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "vshop"
+                }
+
+            }
+
+        };
+
+}
--- Backend/VShop.IdentityServer/Program.cs
+++ Backend/VShop.IdentityServer/Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.IO;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using VShop.IdentityServer;
+using VShop.IdentityServer.Configuration;
 using VShop.IdentityServer.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +21,26 @@ builder.Services.AddControllersWithViews();
 var mySqlConnection = builder.Configuration.GetConnectionString("Default")?.Replace("#PASSWORD#", Environment.GetEnvironmentVariable(VARIABLE_PASSWORD));
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+var builderIdentityServer = builder.Services
+    .AddIdentityServer(options =>
+    {
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+        options.EmitStaticAudienceClaim = true;
+    })
+    .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();
+
+builderIdentityServer.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -34,6 +56,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
